62c0d70b1232a85a8a60720578c3654806712f75
adeleye56 <adeleyeabdulazeez40@gmail.com>
2024-10-21 18:43:29 +0100

    added a new change to my work

--- script.js
+++ script.js
@@ -2,42 +2,60 @@
     let two = document.getElementById('input2')
     let three = document.getElementById('input3')
 
-    function addition() {
+    function calc(operator) {
         let one1 = input1.value
         let two2 = input2.value
-        let three3 = input3.value
 
-        if(one1 || two2 === Number){
-            input3.value = one1 + two2;
+        if (operator === '+'){
+            input3.value = +(one1) + +(two2)
+        } else if (operator === '-'){
+            input3.value = +(one1) - +(two2)
+        }else if (operator === '/'){
+            input3.value = +(one1) / +(two2)
+        } else if (operator === '-'){
+            input3.value = +(one1) - +(two2)
+        }else if (operator === '**'){
+            input3.value = Number(one1) ** Number(two2)
+        }else if (operator === 'sqrt'){
+            input3.value = Number (Math.sqrt(one1))
         }
     }
+    // function addition() {
+    //     let one1 = input1.value
+    //     let two2 = input2.value
+    //     let three3 = input3.value
 
-    function multiplication() {
-        let one1 = input1.value
-        let two2 = input2.value
-        let three3 = input3.value
+    //     if(one1 || two2 === Number){
+    //         input3.value = one1 + two2;
+    //     }
+    // }
 
-        if(one1 || two2 === Number){
-            input3.value = one1 * two2;
-        }
-    }
+    // function multiplication() {
+    //     let one1 = input1.value
+    //     let two2 = input2.value
+    //     let three3 = input3.value
 
-    function division() {
-        let one1 = input1.value
-        let two2 = input2.value
-        let three3 = input3.value
+    //     if(one1 || two2 === Number){
+    //         input3.value = one1 * two2;
+    //     }
+    // }
 
-        if(one1 || two2 === Number){
-            input3.value = one1 / two2;
-        }
-    }
+    // function division() {
+    //     let one1 = input1.value
+    //     let two2 = input2.value
+    //     let three3 = input3.value
+
+    //     if(one1 || two2 === Number){
+    //         input3.value = one1 / two2;
+    //     }
+    // }
     
-    function subtract() {
-        let one1 = input1.value
-        let two2 = input2.value
-        let three3 = input3.value
+    // function subtract() {
+    //     let one1 = input1.value
+    //     let two2 = input2.value
+    //     let three3 = input3.value
 
-        if(one1 || two2 === Number){
-            input3.value = one1 - two2;
-        }
-    }
+    //     if(one1 || two2 === Number){
+    //         input3.value = one1 - two2;
+    //     }
+    // }
